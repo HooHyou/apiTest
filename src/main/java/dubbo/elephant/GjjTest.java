0aa8de92bd49809cffe4ef363e6417af3dd8b2a9
Chenxiaojun <Chenxj@ChenxjdeMacBook-Pro.local>
2017-05-23 12:44:59 +0800

    新增修改

--- src/main/java/dubbo/elephant/GjjTest.java
+++ src/main/java/dubbo/elephant/GjjTest.java
@@ -1,6 +1,7 @@
 package dubbo.elephant;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.google.gson.Gson;
 import com.netease.wyxd.elephant.client.bean.request.gongjijin.GjjDataBean;
@@ -55,6 +56,69 @@ public class GjjTest extends DubboTestBase{
         }
     }
 
+    @Test
+    public void save51GjjInfo(){
+        String data = "{"
+                + "\"gjjinfo\": {"
+                + "\"name\": \"谢xx\","
+                + "\"ID\": \"350***********1823\","
+                + "\"province\": \"浙江\", "
+                + "\"location\": \"杭州\","
+                + "\"location_all\": \"杭州市公积金\", "
+                + "\"city\": \"杭州\","
+                + "\"base\": \"576\","
+                + "\"balance\": \"5760.00\","
+                + "\"deposit_base\": \"2400\","
+                + "\"state\": \"正常\","
+                + "\"record_date\": \"2017-04-25\","
+                + "\"refresh_time\": \"1493728480\","
+                + "\"company\": \"杭州煎饼网络技术有限公司\","
+                + "\"person_rate\": \"0\","
+                + "\"company_rate\": \"0\"},"
+                + "\"gjjdetail\": ["
+                + "{\"company\": \"杭州煎饼网络技术有限公司\","
+                + "\"record_date\": \"2016-07-28\","
+                + "\"op_type\": \"汇缴\","
+                + "\"record_month\": \"201607\","
+                + "\"amount\": \"576\","
+                + "\"balance\": \"576\""
+                + "},"
+                + "{\"company\": \"杭州煎饼网络技术有限公司\","
+                + "\"record_date\": \"2016-08-24\","
+                + "\"op_type\": \"汇缴\","
+                + "\"record_month\": \"201608\","
+                + "\"amount\": \"576\","
+                + "\"balance\": \"1152\"}]}";
+
+        Gson gson = new Gson();
+        TxtReadUtil txtReadUtil = new TxtReadUtil();
+        //HouseFundInfoBean houseFundInfoBean = gson.fromJson(itemStr,HouseFundInfoBean.class);
+        JSONObject datajson = JSON.parseObject(data);
+
+        String json3 = "{\n" +
+                "  \"appCode\": \"pixiu-gongjijindai\",\n" +
+                "  \"appId\": \"pixiu-gongjijindai\",\n" +
+                "  \"applyNo\": \"1079212\",\n" +
+                "  \"eventId\": \"2017011814UA00000431\",\n" +
+                "  \"gjjSourceType\": \"FIFTYONE\",\n" +
+                "  \"gjjIsVerified\": \"NO\"\n" +
+                "}";
+
+        GjjDataBean gjjDataBean = gson.fromJson(json3,GjjDataBean.class);
+        gjjDataBean.setData(datajson);
+        gjjDataBean.setPassword(MD5Util.md5("123"));
+        gjjDataBean.setGjjLocationCode("130100");
+        gjjDataBean.setBid("2017011814UA00000431");
+        gjjDataBean.setApplyDate(new Date());
+
+        try {
+            gjjService.saveGjjData(gjjDataBean);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
 
     @Test
     public void saveHouseFundInfo() {
@@ -65,9 +129,9 @@ public class GjjTest extends DubboTestBase{
         HouseFundInfoBean houseFundInfoBean = JSON.parseObject(itemStr,HouseFundInfoBean.class);
 
         String json3 = "{\n" +
-                "  \"appCode\": \"credit-card\",\n" +
-                "  \"appId\": \"credit-card\",\n" +
-                "  \"applyNo\": \"2017011814UA00000431\",\n" +
+                "  \"appCode\": \"pixiu-gongjijindai\",\n" +
+                "  \"appId\": \"pixiu-gongjijindai\",\n" +
+                "  \"applyNo\": \"1079211\",\n" +
                 "  \"eventId\": \"2017011814UA00000431\",\n" +
                 "  \"gjjSourceType\": \"SHULI\",\n" +
                 "  \"gjjIsVerified\": \"NO\"\n" +
--- src/main/java/dubbo/elephant/bailingdaiTest.java
+++ src/main/java/dubbo/elephant/bailingdaiTest.java
@@ -29,7 +29,7 @@ public class bailingdaiTest extends DubboTestBase{
             DataResponse<Integer> response = consumerFinService.queryWangYiScore(requestBean);
             log.info("返回的描述："+response.getRespDesc());
             log.info("返回的分数："+response.getResult());
-            Assert.assertEquals(response.getRespCode(), "0", "返回值和期望不符：");
+            Assert.assertEquals(response.getRespCode(), "130001", "返回值和期望不符：");
 
         } catch (Exception e) {
             log.error(e.getMessage(), e);
